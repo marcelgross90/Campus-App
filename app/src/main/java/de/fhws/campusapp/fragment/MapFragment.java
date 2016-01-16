6990dfc801495158e9d8ef9c6ae3a7545d21646f
Marcel Groß <mgr@adorsys.de>
2016-01-16 22:24:17 +0100

    added Map and NetworkChangeReceiver

---
+++ app/src/main/java/de/fhws/campusapp/fragment/MapFragment.java
@@ -0,0 +1,52 @@
+package de.fhws.campusapp.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import de.fhws.campusapp.MainActivity;
+
+public class MapFragment extends SupportMapFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+
+    }
+
+    @Override
+    public void onViewCreated( View view, Bundle savedInstanceState ) {
+        super.onViewCreated( view, savedInstanceState );
+
+       setUpMap();
+
+    }
+
+    private synchronized void setUpMap() {
+
+        getMapAsync( new OnMapReadyCallback() {
+            @Override
+            public void onMapReady( GoogleMap googleMap ) {
+                try {
+                    googleMap.setMyLocationEnabled( true );
+                } catch ( SecurityException ex ) {
+                    //die silently
+                }
+                if( MainActivity.location != null ) {
+                    LatLng latlng = new LatLng( MainActivity.location.getLatitude(), MainActivity.location.getLongitude() );
+                    googleMap.moveCamera( CameraUpdateFactory.newLatLng( latlng ) );
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,(float) 14.6));
+                }
+
+            }
+        } );
+    }
+
+
+
+}
