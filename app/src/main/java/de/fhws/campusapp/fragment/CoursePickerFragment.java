967b35c39c432cb66d797b2bea88b073cdeac6f5
Marcel Groß <mgr@adorsys.de>
2016-01-16 19:31:48 +0100

    added filter menu

--- app/src/main/java/de/fhws/campusapp/fragment/CoursePickerFragment.java
+++ app/src/main/java/de/fhws/campusapp/fragment/CoursePickerFragment.java
@@ -5,44 +5,86 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import de.fhws.campusapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class CoursePickerFragment extends DialogFragment {
 
+    private boolean[] choice = new boolean[3];
     private RadioButton bin;
     private RadioButton win;
     private RadioButton ec;
-
 
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate( R.layout.fragment_course_picker, container, false );
-        getDialog().setTitle("My Dialog Title");
+        final View view = inflater.inflate( R.layout.fragment_course_picker, container, false );
+        getDialog().setTitle(R.string.card_caption_study);
+        loadSavedPreferences();
         bin = (RadioButton) view.findViewById( R.id.bin );
         win = (RadioButton) view.findViewById( R.id.win );
         ec = (RadioButton) view.findViewById( R.id.ec );
+
+        bin.setChecked( choice[0] );
+        win.setChecked( choice[1] );
+        ec.setChecked( choice[2] );
+
+        final RadioGroup choiceGroup = (RadioGroup) view.findViewById( R.id.radioButtons );
+
+        choiceGroup.setOnCheckedChangeListener( new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged( RadioGroup group, int checkedId ) {
+
+                switch ( checkedId ) {
+                    case R.id.bin:
+                        choice[0] = true;
+                        choice[1] = false;
+                        choice[2] = false;
+                        break;
+                    case R.id.win:
+                        choice[0] = false;
+                        choice[1] = true;
+                        choice[2] = false;
+                        break;
+                    case R.id.ec:
+                        choice[0] = false;
+                        choice[1] = false;
+                        choice[2] = true;
+                        break;
+                }
+
+
+                savePreferences();
+                dismiss();
+            }
+        } );
 
         return view;
     }
 
 
-    public void savePreferences(boolean[] moduleChoise) {
+    private void loadSavedPreferences(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        for (int i = 0; i < choice.length; i++){
+            choice[i] = sharedPreferences.getBoolean("choice" +i, true);
+        }
+    }
+
+    public void savePreferences() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences( getActivity() );
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        for (int i = 0; i < moduleChoise.length; i++){
-            editor.putBoolean(getString( R.string.app_name ) +i, moduleChoise[i]);
+        for (int i = 0; i <  choice.length; i++){
+            editor.putBoolean("choice" +i, choice[i]);
         }
 
         editor.apply();
