16d0e56e2502423d00aade9b2c74e96c879054fe
Marcel Groß <mgr@adorsys.de>
2016-02-03 20:13:51 +0100

    removed unnecessary imports

--- app/src/main/java/de/fhws/campusapp/network/BaseNetwork.java
+++ app/src/main/java/de/fhws/campusapp/network/BaseNetwork.java
@@ -4,19 +4,15 @@ import android.os.AsyncTask;
 
 import com.google.gson.Gson;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class BaseNetwork {
     public interface OnResultListener {
