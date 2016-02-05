ee6c6cd7029c6e2d5444b8bf487d38cfc3038148
Marcel Groß <mgr@adorsys.de>
2016-02-05 11:31:27 +0100

    Add some database methods

--- app/src/main/java/de/fhws/campusapp/fragment/StatisticFragment.java
+++ app/src/main/java/de/fhws/campusapp/fragment/StatisticFragment.java
@@ -1,19 +1,31 @@
 package de.fhws.campusapp.fragment;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.fhws.campusapp.R;
+import de.fhws.campusapp.database.ModuleDBHelper;
+import de.fhws.campusapp.entity.Module;
 
 public class StatisticFragment extends Fragment {
 
+    private String myChoice = "";
+    private List<Module> modules;
+    private ModuleDBHelper moduleDBHelper;
 
     public StatisticFragment() {
-        // Required empty public constructor
+        this.moduleDBHelper = ModuleDBHelper.getInstance( getActivity() );
+        this.modules = new ArrayList<>(  );
+        addVisitedModules();
     }
 
 
@@ -22,6 +34,27 @@ public class StatisticFragment extends Fragment {
                               Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
         return inflater.inflate( R.layout.fragment_statistic, container, false );
+    }
+
+    
+    private int calculateNumbersOfCourses() {
+        loadSavedPreferences();
+        if( myChoice.equals( Module.Program.BEC ) )
+            return 36;
+        return 35;
+    }
+
+    private void loadSavedPreferences(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences( getActivity() );
+        myChoice = sharedPreferences.getString( "mychoice", Module.Program.BIN );
+    }
+
+    private void addVisitedModules() {
+
+        for ( Module current: moduleDBHelper.readAllModules() ) {
+            if( current.isVisited() )
+                modules.add( current );
+        }
     }
 
 }
