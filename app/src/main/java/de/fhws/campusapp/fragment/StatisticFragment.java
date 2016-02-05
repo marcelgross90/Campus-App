6829b54f970c3974c78c5018b80c52426aa868b3
Christian Braun <chbr@adorsys.de>
2016-02-05 12:41:15 +0100

    added cardview, and added logic to chart

--- app/src/main/java/de/fhws/campusapp/fragment/StatisticFragment.java
+++ app/src/main/java/de/fhws/campusapp/fragment/StatisticFragment.java
@@ -18,8 +18,6 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 
-import java.util.ArrayList;
-
 import de.fhws.campusapp.R;
 import de.fhws.campusapp.database.ModuleDBHelper;
 import de.fhws.campusapp.entity.Module;
@@ -27,7 +25,7 @@ import de.fhws.campusapp.entity.Module;
 public class StatisticFragment extends Fragment {
 
     private String myChoice = "";
-    private List<Module> modules;
+    private List<Module> visitedModules;
     private ModuleDBHelper moduleDBHelper;
 
     private PieChart pieChart;
@@ -38,7 +36,7 @@ public class StatisticFragment extends Fragment {
 
     public StatisticFragment() {
         this.moduleDBHelper = ModuleDBHelper.getInstance( getContext() );
-        this.modules = new ArrayList<>(  );
+        this.visitedModules = new ArrayList<>(  );
         addVisitedModules();
     }
 
@@ -72,8 +70,8 @@ public class StatisticFragment extends Fragment {
 
     private int calculateECTS() {
         int ects = 0;
-        if( modules.size() > 0 ) {
-            for ( Module module : modules ) {
+        if( visitedModules.size() > 0 ) {
+            for ( Module module : visitedModules) {
                 ects += module.getEcts();
             }
         }
@@ -83,10 +81,11 @@ public class StatisticFragment extends Fragment {
 
     private void initPieChart(){
         ArrayList<Entry> dataEntrys = new ArrayList<>();
-        dataEntrys.add(new Entry(30,0));
-        dataEntrys.add(new Entry(50, 0));
+        dataEntrys.add(new Entry(visitedModules.size(),0));
+        dataEntrys.add(new Entry(calculateNumbersOfCourses() - visitedModules.size(), 1));
 
         PieDataSet dataSet = new PieDataSet(dataEntrys, getResources().getString(R.string.modules));
+        dataSet.setValueTextSize(15);
         dataSet.setColors(new int[] { R.color.charColorOne, R.color.charColorTwo }, getContext());
 
         ArrayList<String>titles = new ArrayList<String>();
@@ -112,7 +111,7 @@ public class StatisticFragment extends Fragment {
 
         for ( Module current: moduleDBHelper.readAllModules() ) {
             if( current.isVisited() )
-                modules.add( current );
+                visitedModules.add( current );
         }
     }
 
